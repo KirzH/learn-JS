4472283110e7e145641b4e9c985a78b42b579ed1
Вадим Киржаков <@kirzhakov.2020>
2021-05-06 15:59:18 +0300

    fix main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+// const foo = [1, 2]
+// const bar = foo
+
+// bar[0] = 15
+
+// console.log(foo[0])
+
+// const arrLike = { 0: 'foo', 1: 'bar', 2: 'baz', length: 3 };
+// const arr = Array.from(arrLike);
+// console.log(arr)
+
+
+// function getFullName({ firstName, lastName }) {
+//     return `${firstName} ${lastName}`;
+
+// const arr = [1, 2, 3, 4];
+// const [first, second] = arr;
+
+// (function () {
+//     console.log('Welcome to the Internet. Please follow me.');
+//   }());
+
+//   let test;
+// if (currentUser) {
+//   test = () => {
+//     console.log('Yup.');
+//   };
+// }
+
+
